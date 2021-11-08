43888188c450d39b2c2ca41ccf4081b2865f664d
vnakonechnuy <vnakonechnuy@i.ua>
2021-11-08 23:33:09 +0200

    Исправлен conftest.py

---
+++ conftest.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None , help='Выбери язык браузера')
+
+
+@pytest.fixture()
+def browser(request):
+    browser_language = request.config.getoption('language')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+    browser = webdriver.Chrome(options=options)
+    language = browser.execute_script("return window.navigator.userLanguage || window.navigator.language")
+    print('..:: STARTING BROWSER ::..')
+
+    yield browser
+    print('..:: CLOSING BROWSER ::..')
+    browser.quit()
